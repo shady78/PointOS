c4957c25aca8715214f790770687aa16b0d335de
shadykh <shadykh86@gmail.com>
2025-05-04 15:37:02 +0300

    Add project files.

---
+++ PointOS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PointOS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
